06815607d108c9c687c9dcbbfac6a7e4c763842b
Edwin Romero <edwinrmrjr@gmail.com>
2024-03-21 00:42:20 +0800

    adding signal with agora

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 let APP_ID = "1cfcddb419384cc5b417ed14f38d8eda"
 
+let token = null;
+let uid = String(Math.floor(Math.random() * 10000))
+
+let client;
+let channel;
+
 let localStream;
 let remoteStream;
 let peerConnection;
@@ -13,17 +19,40 @@ const servers = {
 }
 
 let init = async () => {
+    client = await AgoraRTM.createInstance(APP_ID)
+    await client.login({uid, token})
+
+    channel = client.createChannel('main')
+    await channel.join()
+
+    channel.on('MemberJoined', handleUserJoined)
+
+    client.on('MessageFromPeer', handleMessageFromPeer)
+
     localStream = await navigator.mediaDevices.getUserMedia({video:true, audio:false})
     document.getElementById('user-1').srcObject = localStream
-
-    createOffer()
 }
 
-let createOffer = async () => {
+let handleMessageFromPeer = async (message, MemberId) => {
+    message = JSON.parse(message.text)
+    console.log('Message:', message)
+}
+
+let handleUserJoined = async (MemberId) => {
+    console.log('A new user joined the channel:', MemberId)
+    createOffer(MemberId)
+}
+
+let createOffer = async (MemberId) => {
     peerConnection = new RTCPeerConnection(servers)
 
     remoteStream = new MediaStream()
     document.getElementById('user-2').srcObject = remoteStream
+
+    if(!localStream){
+        localStream = await navigator.mediaDevices.getUserMedia({video:true, audio:false})
+        document.getElementById('user-1').srcObject = localStream
+    }
 
     localStream.getTracks().forEach((track) => {
         peerConnection.addTrack(track, localStream)
@@ -37,15 +66,14 @@ let createOffer = async () => {
 
     peerConnection.onicecandidate = async (event) => {
         if(event.candidate){
-            console.log('New ICE candidate:', event.candidate)
+            client.sendMessageToPeer({text:JSON.stringify({'type': 'candidate', 'candidate': event.candidate})}, MemberId)
         }
     }
 
     let offer = await peerConnection.createOffer()
     await peerConnection.setLocalDescription(offer)
 
-    console.log('Offer:', offer)
-
+    client.sendMessageToPeer({text:JSON.stringify({'type': 'offer', 'offer': offer})}, MemberId)
 }
 
 init()
